8f375c459c61a635d3ab9b332e097c02fa781f52
Habib <sa27021984@gmail.com>
2026-01-21 02:24:09 +0100

    added the filter logic and frontend

--- frontend/src/pages/EmployeePage.jsx
+++ frontend/src/pages/EmployeePage.jsx
@@ -10,24 +10,71 @@ import TableJudicial from '../components/TableJudicial';
 function EmployeePage() {
       const [employees, setEmployees] = useState([]);
       const [judicialEmployees, setJudicialEmployees] = useState([]);
+
       const [companyFilter, setCompanyFilter] = useState(0);
       const [judicialFilter, setJudicialFilter] = useState(0);
+
+      const [cityFilter, setCityFilter] = useState("all");
+      const [judicialCityFilter, setJudicialCityFilter] = useState("all");
+
+      const [departement, setDepartement] = useState("all");
+
     
-        const options = [
-        { label: "All Departments", value: 0 },
-        { label: "حفظ الأرشيف", value: 1 },
-        { label: "الموارد البشرية", value: 2 },
-        { label: "التجهيز و نظم المعلومات", value: 3 },
-      ];
+    const options = [
+      { label: "All Departments", value: 0 },
+      { label: "حفظ الأرشيف", value: 1 },
+      { label: "الموارد البشرية", value: 2 },
+      { label: "التجهيز و نظم المعلومات", value: 3 },
+    ];
     
-      const judicialOptions = [
+    const judicialOptions = [
       { label: "All Entities", value: 0 },
       { label: "محكمة الاستئناف", value: "محكمة الاستئناف" },
       { label: "المحكمة الابتدائية", value: "المحكمة الابتدائية" },
       { label: "المركز القضائي", value: "المركز القضائي" },
     ];
-    
-    
+
+    const cityOptions = [
+      { label: "All Cities", value: "all" },
+      { label: "laayoune", value: "laayoune" },
+      { label: "Smara", value: "Smara" },
+      { label: "Boujdour", value: "Boujdour" },
+      { label: "Dakhla", value: "Dakhla" },
+    ];
+    const departementOption = [
+      { label: "All Departments", value: "all" },
+      { label: "النيابة العامة", value: "النيابة العامة" },
+      { label: "رئاسة", value: "رئاسة" },
+    ]
+
+    // company employee : 
+    const filteredCompanyEmployees = employees.filter(emp => {
+      const departmentMatch =
+        companyFilter === 0 || Number(emp.departement_id) === companyFilter;
+
+      const cityMatch =
+        cityFilter === "all" || emp.nom_ville === cityFilter;
+
+      return departmentMatch && cityMatch;
+    });
+
+    // judicial employee : 
+    const filteredJudicialEmployees = judicialEmployees.filter(emp => {
+  const entityMatch =
+    judicialFilter === 0 || emp.entity_type === judicialFilter;
+
+  const cityMatch =
+    judicialCityFilter === "all" ||
+    emp.nom_ville?.toLowerCase() === judicialCityFilter.toLowerCase();
+
+  const departementMatch =
+    departement === "all" || emp.department === departement;
+
+  return entityMatch && cityMatch && departementMatch;
+});
+
+
+
       useEffect(() => {
         api.get("/companyEmployes")
           .then((res) => setEmployees(res.data))
@@ -38,33 +85,23 @@ function EmployeePage() {
           .then((res) => setJudicialEmployees(res.data))
           .catch((error) => console.error(error));
       }, []);
-      
-      const filteredCompanyEmployees =
-        companyFilter === 0
-          ? employees
-          : employees.filter(
-              emp => Number(emp.departement_id) === companyFilter
-            );
-    
-      const filteredJudicialEmployees =
-        judicialFilter === 0
-          ? judicialEmployees
-          : judicialEmployees.filter(
-              emp => emp.entity_type === judicialFilter
-            );
   return (
         <>
     <NavbarHeader />
     
     <div className="toolbar">
       <DropDown options={options} onChange={setCompanyFilter} />
+      <DropDown options={cityOptions} onChange={setCityFilter} />
     </div>
     <div>
       <Table emp={filteredCompanyEmployees} />
     </div>
     <div className="toolbar">
       <DropDown options={judicialOptions} onChange={setJudicialFilter} />
+      <DropDown options={cityOptions} onChange={setJudicialCityFilter} />
+      <DropDown options={departementOption} onChange={setDepartement} />
     </div>
+
     <div>
       <TableJudicial emp={filteredJudicialEmployees} />
     </div>
